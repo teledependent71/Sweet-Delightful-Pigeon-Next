9bc149992cf898e27bb6f733669b8cc842629fc2
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2023-09-28 18:38:01 +0300

    Commit from TeleportHQ

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Head from 'next/head'
+
+const Home = (props) => {
+  return (
+    <>
+      <div className="home-container">
+        <Head>
+          <title>Sweet Delightful Pigeon</title>
+          <meta property="og:title" content="Sweet Delightful Pigeon" />
+        </Head>
+      </div>
+      <style jsx>
+        {`
+          .home-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default Home
